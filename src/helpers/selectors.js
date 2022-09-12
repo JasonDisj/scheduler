10186b017c0f3ee88d2114f49af03d812a321d5a
Jason Di <jasondisj@gmail.com>
2022-09-12 09:31:16 -0700

    Initial commit

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,3 +9,28 @@ export function getAppointmentsForDay(state, day) {
   }
   return appointmentsForDay;
 }
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+
+  const interviewer = state.interviewers[interview.interviewer];
+  return {
+    // return a student name and an interviewer obj
+    ...interview,
+    interviewer,
+  };
+}
+
+export function getInterviewersForDay(state, day) {
+  const interviewersForDay = [];
+  for (const randomDay of state.days) {
+    if (randomDay.name === day) {
+      randomDay.interviewers.forEach((interviewerId) => {
+        interviewersForDay.push(state.interviewers[interviewerId]);
+      });
+    }
+  }
+  return interviewersForDay;
+}
