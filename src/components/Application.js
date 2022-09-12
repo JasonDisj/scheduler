45bd0dafc88ac7f09350887d7a82ad643fb59fdc
Jason Di <jasondisj@gmail.com>
2022-09-12 16:17:54 -0700

    Organized by managing the state into useApplicationData Hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
+import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment/index";
@@ -9,36 +7,13 @@ import {
   getInterview,
   getInterviewersForDay,
 } from "../helpers/selectors";
-import useVisualMode from "hooks/useVisualMode";
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
-  // const [day, setDay] = useState("Monday");
-  // const [days, setDays] = useState([]);
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {},
-  }); // combined state
+  const { state, setDay, bookInterview, cancelInterview } =
+    useApplicationData();
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-
-  const setDay = (day) => setState((prev) => ({ ...prev, day: day }));
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers"),
-    ]).then((all) => {
-      setState((prev) => ({
-        ...prev,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data,
-      }));
-    });
-  }, []);
 
   return (
     <main className="layout">
@@ -70,6 +45,8 @@ export default function Application(props) {
               time={appointment.time}
               interview={interview}
               interviewers={interviewersForDay}
+              bookInterview={bookInterview}
+              cancelInterview={cancelInterview}
             />
           );
         })}
