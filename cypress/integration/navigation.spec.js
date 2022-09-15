9c48c50492278adcce918ce1eeb6c5d6d75fe7b5
Jason Di <jasondisj@gmail.com>
2022-09-14 17:17:23 -0700

    Initial commit

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,25 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    // cy.get("li")
+    //   .contains("Tuesday")
+    //   .should("have.css", "background-color", "rgb(242, 242, 242)");
+
+    // cy.contains("li", "Tuesday").should(
+    //   "have.css",
+    //   "background-color",
+    //   "rgb(242, 242, 242)"
+    // );
+
+    // cy.contains("li", "Tuesday")
+    // .click()
+    // .should("have.css", "background-color", "rgb(242, 242, 242)");
+
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
+  });
+});
